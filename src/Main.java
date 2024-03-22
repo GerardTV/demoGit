2604465a1af57ea2d8772a2486aad0e2f4ccd26d
Roger <belegund@gmail.com>
2024-03-22 13:00:09 +0100

    Roger ha comentado la línea 5 de Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Nuevo cambio");
-        System.out.println("Cambio en develop");
+        // System.out.println("Cambio en develop"); // Roger ha comentado la línea 5
         System.out.println("Cambio de Silvia");
         System.out.println("Soy Sammy y he realizado un cambio");
         System.out.println("Otro cambio más");
